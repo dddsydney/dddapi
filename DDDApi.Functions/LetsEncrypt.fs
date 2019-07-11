a66b56d57e59a64ef7af320decb0a71c860f0270
Aaron Powell <me@aaron-powell.com>
2019-07-11 16:11:58 +1000

    Adding lets encrypt function

---
+++ DDDApi.Functions/LetsEncrypt.fs
@@ -0,0 +1,23 @@
+module LetsEncrypt
+
+open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs.Extensions.Http
+open Microsoft.AspNetCore.Http
+open System.IO
+open Microsoft.AspNetCore.Mvc
+
+[<FunctionName("Lets_Encrypt")>]
+let letsEncrypt
+    ([<HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "letsencrypt/{code}")>] req: HttpRequest)
+    (context: ExecutionContext)
+    (code: string) =
+
+    let path = Path.Combine(context.FunctionDirectory, ".well-known", "acme-challenge", code)
+    
+    match File.Exists path with
+    | true ->
+        let res = path
+                    |> File.ReadAllText
+                    |> JsonResult
+        res :> IActionResult
+    | false -> NotFoundResult() :> IActionResult
